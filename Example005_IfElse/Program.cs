ba241afeb03a2e894208e473d5c016e52b61907e
boris <bova4fa@gmail.com>
2023-02-10 14:57:25 +0300

    add Examples 003 selected user

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Ура, это же Маша!");
+}
+else{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
