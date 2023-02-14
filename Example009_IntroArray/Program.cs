442ebc94fbcd9864ef99f9a1c41c2509700e1e56
boris <bova4fa@gmail.com>
2023-02-14 12:56:04 +0300

    Examples of L2 - methods & arrays

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,23 @@
+﻿// Intro of the methods (functions)
+//              0  1  2  3  4  5  6  6  8
+int[] array = {11,21,31,41,15,61,17,18,19};
+
+ int Max (int arg1, int arg2, int arg3)
+ {
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+
+    return result;
+ }   
+
+// int Max1 = Max(a1, b1, c1);
+// int Max2 = Max(a2, b2, c2);
+// int Max3 = Max(a3, b3, c3);
+
+//Console.WriteLine(Max(Max1, Max2, Max3));
+Console.WriteLine(Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8]))
+    );
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿// set random array and finds an index of element equal to preset 'find' using method
+void FillArray(int[] collection)
+{
+    int lng = collection.Length;
+    int index = 0;
+    while(index < lng)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int lng = col.Length;
+    int index = 0;
+    while(index < lng)
+    {
+        Console.WriteLine(col[index]);
+        index++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int sz = collection.Length;
+    int index = 0;
+    int position = -1; //-1 to signal if no preset values are found in the array
+
+    while(index < sz)
+    {
+        if(collection[index] == find)
+        {
+            position = index + 1;
+            break;
+        }
+        index++;
+    }
+
+    return position;
+}
+
+int find = 4;
+int[] array = new int[10]; //0 array of a size 10
+
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine(IndexOf(array, find));
+            
