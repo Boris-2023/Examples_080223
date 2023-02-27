9df0eab394b43af5507fcba357f32f59933ebe25
boris <bova4fa@gmail.com>
2023-02-27 15:56:26 +0300

    Lecture 4 - 2d arrays & RECURSION

---
+++ Example013_RecursionAlgo/Program.cs
@@ -0,0 +1,125 @@
+﻿// 2d arrays & recursion
+
+string[,] table = new string[2, 5];//each cell contains a full string! Output by i-j cycles; indexes starts at 0
+
+int[,] matrix = new int[5, 8];
+
+void print2DArrayInt(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Fill2DArrayRandInt(int[,] matr, int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);//+1 as initially Random takes as [min,max) !!!
+        }
+    }
+}
+
+
+print2DArrayInt(matrix);
+Console.WriteLine();
+Fill2DArrayRandInt(matrix, 1, 10);
+print2DArrayInt(matrix);
+
+
+//fill the picture by RECURSION
+int[,] pic = new int[,] //mtx of the picture
+{
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+void ImageArrayInt(int[,] matr)//draw a p[icture by 0-1 matrix
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            //Console.Write($"{matr[i, j]}\t");
+            if (matr[i, j] == 0) Console.Write($"  ");
+            else Console.Write($"++");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImageByRecursion(int row, int col)//arguments - start point
+{
+    if (pic[row, col] == 0)//not filled
+    {
+        pic[row, col] = 1;
+        FillImageByRecursion(row - 1, col);//then move up
+        FillImageByRecursion(row, col - 1);//then move left
+        FillImageByRecursion(row + 1, col);//then move down
+        FillImageByRecursion(row, col + 1);//then move right
+    }
+}
+
+ImageArrayInt(pic);
+
+FillImageByRecursion(13, 13);
+ImageArrayInt(pic);
+
+
+//Factorial by Recursion
+
+double Factorial(int n)
+{
+    //1! == 1, 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(20));//1*2*3 = 6
+
+
+
+//Fibonacci row by Recursion: f(1)=1, f(2)=1,...,f(n) = f(n-1) + f(n-2);
+
+double Fibo(int n)
+{
+    if(n==1 || n==2) return 1;
+    else return Fibo(n-1) + Fibo(n-2);
+}
+
+Console.WriteLine();
+for(int i = 1; i<=40; i++)
+{
+    Console.WriteLine($"f({i}) = \t{Fibo(i)}");
+}
+
+
+//Overview a folder
