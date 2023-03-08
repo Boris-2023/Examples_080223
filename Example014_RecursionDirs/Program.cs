3fb4206149b2c292bbffc7a5708b710c878ab8fc
boris <bova4fa@gmail.com>
2023-03-08 17:02:27 +0300

    Lecture 7 - Recursion, 06 Mar 2023

---
+++ Example014_RecursionDirs/Program.cs
@@ -0,0 +1,34 @@
+﻿// algorythm of directory overview by recursion; all files & folders
+
+string path = @"/home/boris/GB/Programming languages basics C#/Lectures 1/Examples/Example001_HelloConsole";//sets path of the folder
+DirectoryInfo di = new DirectoryInfo(path); //info on the directory
+
+Console.WriteLine(di.CreationTime);//e.g. time of creation
+
+FileInfo[] fi = di.GetFiles();//array of files features
+
+for (int i = 0; i < fi.Length; i++)//for all the files in the array/folder
+{
+    Console.WriteLine(fi[i].Name);//prints name
+}
+
+void CatalogInfo(string path, string indent = "")//arguments: path for the folder & a serv symbol for layout inside folder/files
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+
+}
+
+CatalogInfo(path);
---
+++ Example015_PyramidsExchange/Program.cs
@@ -0,0 +1,10 @@
+﻿// exchange disks from stick 1 to stick 3 using stick 2 (babies' pyramids)
+
+void Towers(string from = "1", string to = "3", string with = "2", int disksNum = 3)// disksNum - number of disks to transfer
+{
+    if(disksNum>1) Towers(from, with, to, disksNum-1);
+    Console.WriteLine($"{from} >> {to}");
+    if(disksNum>1) Towers(with, to, from, disksNum-1);
+}
+
+Towers();
